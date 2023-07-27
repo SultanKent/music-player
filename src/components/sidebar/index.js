18553d1052cd9291bb25879a0284027133d8a44c
Sultan <ffkirito6@gmail.com>
2023-07-27 12:56:33 +0600

    401 bug was fixed

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -7,22 +7,23 @@ import { FaSignOutAlt } from "react-icons/fa";
 import { IoLibrary } from "react-icons/io5";
 import { MdSpaceDashboard } from "react-icons/md";
 import apiClient from "../../spotify";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Sidebar() {
   const navigate = useNavigate();
   const [image, setImage] = useState(
     "https://www.pngplay.com/wp-content/uploads/12/Killua-Zoldyck-PNG-Images-HD.png"
   );
-  useEffect(() => {
-    apiClient.get("me").then((response) => {
-      setImage(response.data.images[0].url);
-    });
-  }, []);
+  const [tokenExists, setTokenExists] = useState(Boolean(localStorage.getItem("token")));
   const handleSignOut = () => {
     window.localStorage.removeItem("token");
-    navigate("/");
+    setTokenExists(false);
+    apiClient.resetState();
   };
+  useEffect(() => {
+    setTokenExists(Boolean(localStorage.getItem("token")));
+  }, []);
+
   return (
     <div className="sidebar-container">
       <img src={image} className="profile-img" alt="profile" />
@@ -32,7 +33,12 @@ export default function Sidebar() {
         <SidebarButton title="Player" to="/player" icon={<FaPlay />} />
         <SidebarButton title="Library" to="/" icon={<IoLibrary />} />
       </div>
-      <SidebarButton title="Sign Out" to='/' icon={<FaSignOutAlt />} onClick={handleSignOut}/>
+      {/* Disable the "Sign Out" button when token doesn't exist */}
+      <button onClick={handleSignOut}
+      style={{border: 'none', background: 'transparent'}}
+      className="btn-body active">
+        <FaSignOutAlt style={{width: '50px', height: '50px'}}/>
+      </button>
     </div>
   );
 }
